d1747e0cd1c2528c3149905923b57c6192a62c09
Marty <rosary-vassals8m@icloud.com>
2025-08-26 08:45:28 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'worktime-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/service-worker.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(
+      resp => resp || fetch(event.request)
+    )
+  );
+});
